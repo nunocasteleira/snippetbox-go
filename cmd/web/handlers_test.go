bd0ef30ecc696ef2b20792fd7d53937e54811d7d
Nuno Casteleira <nunojoaocas@gmail.com>
2025-03-31 14:46:51 +0100

    add test utils

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox-go.casteleira.xyz/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
-
+	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, string(body), "OK")
 }
